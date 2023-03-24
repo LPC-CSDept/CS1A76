3a619aeffbe988a67f916f93aecc1114af6f5761
Kayw Lee <kwooler@gmail.com>
2023-03-23 18:24:40 -0700

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "main.hpp"
+const int N = 5000;
+int main()
+{
+	string state[N];
+	string gender[N];
+	int year[N];
+	string name[N];
+	int count[N];
+	int cnt;
+	char starting = 'J';
+	string stname = "TX";
+
+	cnt = makeNameRecord(state, gender, year, name, count);
+	printoutallrecords(cnt, state, gender, year, name, count);
+
+	cnt = findNames(cnt, state, gender, year, name, count, starting, stname);
+}
